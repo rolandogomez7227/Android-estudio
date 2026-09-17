6d5fcf3064a653ff2f87318e2f02a9f3bb44ab87
rolando <rolandogomez7227@gmail.com>
2026-09-17 11:10:09 -0600

    Coneccion

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -20,13 +20,50 @@ class LoginActivity : AppCompatActivity() {
             val correo = etCorreo.text.toString()
             val password = etPassword.text.toString()
 
-            if(correo.isNotEmpty() && password.isNotEmpty()) {
-                // Ir a la página de inicio
-                val intent = Intent(this, HomeActivity::class.java)
-                startActivity(intent)
-                finish()
+            if (correo.isNotEmpty() && password.isNotEmpty()) {
+                val retrofit = retrofit2.Retrofit.Builder()
+                    .baseUrl("http://10.0.2.2:8000/")
+                    .addConverterFactory(retrofit2.converter.gson.GsonConverterFactory.create())
+                    .build()
+
+                val api = retrofit.create(ApiService::class.java)
+                val request = LoginRequest(correo, password)
+
+                api.login(request).enqueue(object : retrofit2.Callback<LoginResponse> {
+                    override fun onResponse(
+                        call: retrofit2.Call<LoginResponse>,
+                        response: retrofit2.Response<LoginResponse>
+                    ) {
+                        if (response.isSuccessful) {
+                            android.widget.Toast.makeText(
+                                this@LoginActivity,
+                                "¡Sesión iniciada!",
+                                android.widget.Toast.LENGTH_SHORT
+                            ).show()
+
+                        } else {
+                            android.widget.Toast.makeText(
+                                this@LoginActivity,
+                                "Credenciales incorrectas",
+                                android.widget.Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+
+                    override fun onFailure(call: retrofit2.Call<LoginResponse>, t: Throwable) {
+                        android.widget.Toast.makeText(
+                            this@LoginActivity,
+                            "Error de red: ${t.message}",
+                            android.widget.Toast.LENGTH_LONG
+                        ).show()
+                    }
+                })
             } else {
-                Toast.makeText(this, "Llena todos los campos", Toast.LENGTH_SHORT).show()
+                android.widget.Toast.makeText(
+                    this@LoginActivity,
+                    "Llena todos los campos",
+                    android.widget.Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
