303a46efde9dbd2ab1fe06e866769f8f0cb6f8de
rolando <rolandogomez7227@gmail.com>
2026-09-17 14:41:27 -0600

    importacion

--- app/src/main/java/com/example/myapplication/Inicio.kt
+++ app/src/main/java/com/example/myapplication/Inicio.kt
@@ -1,8 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
