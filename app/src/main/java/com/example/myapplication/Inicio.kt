e29cdcfa9b904d29b586129c2ee20a9275ca8e17
rolando <rolandogomez7227@gmail.com>
2026-09-17 14:41:00 -0600

    class

--- app/src/main/java/com/example/myapplication/Inicio.kt
+++ app/src/main/java/com/example/myapplication/Inicio.kt
@@ -6,7 +6,7 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 
-class RutinaActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rutina)
