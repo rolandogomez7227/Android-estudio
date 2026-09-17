9b5a73574b1052563e77c2e89ea4ff89dc3403b3
rolando <rolandogomez7227@gmail.com>
2026-09-17 14:51:26 -0600

    cambio de ip

--- app/src/main/java/com/example/myapplication/Inicio.kt
+++ app/src/main/java/com/example/myapplication/Inicio.kt
@@ -20,7 +20,7 @@ class HomeActivity : AppCompatActivity() {
         rvRutinas.layoutManager = LinearLayoutManager(this)
 
         val retrofit = Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:8000/")
+            .baseUrl("http://127.0.0.1:8000/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
